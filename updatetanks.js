afe7fb4061f45d3e33d906bd8d1fd2b346787d1c
Askild Tønne <askild.tonne@tieto.com>
2019-05-09 09:01:16 +0200

    Colorize console.log-ging

--- updatetanks.js
+++ updatetanks.js
@@ -12,6 +12,14 @@ var originalTanksToKeep = [
     "jamro"
 ];
 
+const consoleColors = {
+    Reset: "\x1b[0m",
+    Cyan: "\x1b[36m",
+    Blue: "\x1b[34m",
+    Magenta: "\x1b[35m",
+    Green: "\x1b[32m"
+};
+
 var pathToTanks = 'node_modules/jsbattle/dist/public/tanks';
 var tankRegex = /(.+?)\.tank\.js$/;
 
@@ -32,21 +40,24 @@ const backupFolder = path.join(pathToTanks, `backup_${timeStamp}`);
 
 console.log({backupFolder});
 
+console.log('\n');
 filesToMove.forEach(fileToMove => {
     const source = path.join(pathToTanks, fileToMove);
     const target = path.join(backupFolder, fileToMove);
-    console.log(`Moving: ${source} -> ${target}`);
+    console.log(consoleColors.Magenta, `Moving: ${source} -> ${target}`, consoleColors.Reset);
     fse.moveSync(source, target);
 })
 
 // Copy new tank files
+console.log('\n');
 const newTanks = fse.readdirSync('new-tanks');
 newTanks.forEach(newTank => {
     const source = path.join('new-tanks', newTank);
     const dest = path.join(pathToTanks, newTank);
-    console.log(`Copying: ${source} -> ${dest}`)
+    console.log(consoleColors.Cyan, `Copying: ${source} -> ${dest}`, consoleColors.Reset);
     fse.copySync(source, dest);
 })
+console.log('\n');
 
 const updatedDir = fse.readdirSync(pathToTanks);
 const updatedTankFileNames = updatedDir.filter(str => tankRegex.test(str));
@@ -59,3 +70,5 @@ const updatedTankNames = updatedTankFileNames.map(tankFileName => {
 console.log({updatedTankNames});
 
 fse.outputJSONSync(path.join(pathToTanks, 'index.json'), updatedTankNames);
+
+console.log(consoleColors.Green, '\nDone!', consoleColors.Reset);
